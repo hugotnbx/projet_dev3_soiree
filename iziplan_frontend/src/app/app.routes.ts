ccaf4320355138245a841ad701162287a1431499
0ropo <he202225@students.ephec.be>
2024-03-07 23:48:47 +0100

    jout route 1

--- iziplan_frontend/src/app/app.routes.ts
+++ iziplan_frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AccueilComponent} from "./components/accueil/accueil.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component:AccueilComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
