2637941393a691c6d995f0282e2c868ae8973c7a
0ropo <he202225@students.ephec.be>
2024-03-12 00:35:10 +0100

    La route était longue mais c'est fait
    (Fine ferns and fine jays
    Dig fat frail jade and grave flames
    Flat dunes fear dim kings
    
    Long chemin fut parcouru ici
    Dans nos pensées évasifs on a trouvé
    Un moyen d'échapper au fatjade)
    Ft. Le H, Huglock

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { EvenementComponent } from './components/evenement/evenement.component';
+import { CreationComponent } from './components/creation/creation.component';
+import { InvitationsComponent } from './components/invitations/invitations.component';
+import { ProfilComponent } from './components/profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    AccueilComponent,
+    EvenementComponent,
+    CreationComponent,
+    InvitationsComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
