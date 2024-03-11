902ea2cc9d58bf1e45030ac6748e2dfcf9afd338
Hugo Troonbeeckx <h.troonbeeckx@students.ephec.be>
2024-03-11 16:54:52 +0100

    Réglages de bugs

--- iziplan_client.sql
+++ iziplan_client.sql
@@ -4,18 +4,17 @@ CREATE TABLE client (
     prenom VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     numero_telephone VARCHAR(20),
-    
 );
 
 CREATE TABLE event(
     id int PRIMARY KEY,
     nom varchar(50) NOT NULL,
     date DATE NOT NULL,
-    heure TIMe NOT NULL,
+    heure TIME NOT NULL,
     lieu VARCHAR(100) NOT NULL
 );
 
-create table event_client(
+CREATE TABLE event_client(
     id_event int FOREIGN KEY REFERENCES event(id), 
     id_client int FOREIGN KEY REFERENCES client(id), 
     PRIMARY KEY(id_event, id_client)
