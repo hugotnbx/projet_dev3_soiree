284971f92ca8e4a53273ad06bb207f89e2a9de5d
0ropo <he202225@students.ephec.be>
2024-03-17 22:14:33 +0100

    création event

---
+++ mobile/src/app/components/profil/creation-profil/creation-profil.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-profil',
+  templateUrl: './creation-profil.component.html',
+  styleUrls: ['./creation-profil.component.scss'],
+})
+export class CreationProfilComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
