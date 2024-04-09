95bd1f7e12c3b22253417194419a4f908a82d67f
Hugo Troonbeeckx <h.troonbeeckx@students.ephec.be>
2024-04-09 22:41:20 +0200

    amélioration US 2.14

--- mobile/src/app/components/evenement/evenement.component.spec.ts
+++ mobile/src/app/components/evenement/evenement.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { EvenementComponent } from './evenement.component';
 
 describe('EvenementComponent', () => {
@@ -9,16 +8,39 @@ describe('EvenementComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ EvenementComponent ],
+      declarations: [EvenementComponent],
       imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(EvenementComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(EvenementComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return correct image URL based on role', () => {
+    // Test pour le rôle 'admin'
+    expect(component.getImageUrl('admin')).toEqual('./assets/role/admin.png');
+
+    // Test pour le rôle 'Bob'
+    expect(component.getImageUrl('Bob')).toEqual('./assets/role/bob.png');
+
+    // Test pour le rôle 'place_voiture'
+    expect(component.getImageUrl('place_voiture')).toEqual('./assets/role/place_voiture.png');
+
+    // Test pour le rôle 'hote'
+    expect(component.getImageUrl('hote')).toEqual('./assets/role/hote.png');
+
+    // Test pour le rôle 'lit_dispo'
+    expect(component.getImageUrl('lit_dispo')).toEqual('./assets/role/lit.png');
+
+    // Test pour le rôle 'covoiturage'
+    expect(component.getImageUrl('covoiturage')).toEqual('./assets/role/covoiturage.png');
+
+    // Test pour un rôle non reconnu
+    expect(component.getImageUrl('autre')).toEqual('./assets/role/pas_de_role.png');
   });
 });
--- mobile/src/app/components/evenement/evenement.component.ts
+++ mobile/src/app/components/evenement/evenement.component.ts
@@ -39,7 +39,7 @@ export class EvenementComponent  implements OnInit {
   getImageUrl(role: string): string {
     // Logique pour déterminer l'URL de l'image en fonction du rôle
     // Par exemple :
-    if (role === 'Admin') {
+    if (role === 'admin') {
       return './assets/role/admin.png'; // Chemin de l'image pour le rôle Admin
     } else if (role === 'Bob') {
       return './assets/role/bob.png'; // Chemin de l'image pour le rôle User
@@ -49,7 +49,7 @@ export class EvenementComponent  implements OnInit {
       return './assets/role/hote.png'; // Chemin de l'image pour le rôle User
     } else if (role === 'lit_dispo') {
       return './assets/role/lit.png'; // Chemin de l'image pour le rôle User
-    } else if (role === 'lit_dispo'){
+    } else if (role === 'covoiturage'){
       return './assets/role/covoiturage.png'; // Image par défaut si aucun rôle ne correspond
     } else {
       return './assets/role/pas_de_role.png'
