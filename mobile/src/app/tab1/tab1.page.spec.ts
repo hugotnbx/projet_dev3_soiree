855f1e6fc1ef650c7c2c46aa87a300dd6aeede47
alexandrebrugger <superalexbrugger@gmail.com>
2024-04-15 22:03:30 +0200

    Formatage Date et Heure dans Modification Event + modifications dans test unitairres tab1.spec et tab5.spec

--- mobile/src/app/tab1/tab1.page.spec.ts
+++ mobile/src/app/tab1/tab1.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
@@ -23,4 +23,73 @@ describe('Tab1Page', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});*/
+
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Tab1Page } from './tab1.page';
+
+describe('Tab1Page', () => {
+  let component: Tab1Page;
+  let fixture: ComponentFixture<Tab1Page>;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [Tab1Page],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Tab1Page);
+    component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+    fixture.detectChanges();
+  }));
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should fetch event data from API', () => {
+    const testData = [
+      {
+        "id": 37,
+        "nom": "antony",
+        "heure": "18:26",
+        "date": "2024-04-27",
+        "lieu": "dzd",
+        "nbrLit": 0,
+        "nbrBob": 0
+      }
+    ];
+
+    component.readApi("http://localhost:64000/events").subscribe((data) => {
+      expect(data).toEqual(testData);
+      expect(component.events).toEqual(testData);
+    });
+
+    const req = httpTestingController.expectOne("http://localhost:64000/events");
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
+  });
+
+  /*it('should handle API error', () => {
+    const errorResponse = { status: 404, statusText: 'Not Found' };
+
+    component.readApi("http://localhost:64000/users/hugoneutre").subscribe(
+      () => {},
+      (error) => {
+        expect(error).toEqual(errorResponse);
+      }
+    );
+
+    const req = httpTestingController.expectOne("http://localhost:64000/users/hugoneutre");
+    expect(req.request.method).toEqual('GET');
+    req.flush(null, errorResponse);
+  });*/
 });
--- mobile/src/app/tab5/tab5.page.spec.ts
+++ mobile/src/app/tab5/tab5.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
@@ -18,6 +18,37 @@ describe('Tab5Page', () => {
     fixture = TestBed.createComponent(Tab5Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});*/
+
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Tab5Page } from './tab5.page';
+
+describe('Tab5Page', () => {
+  let component: Tab5Page;
+  let fixture: ComponentFixture<Tab5Page>;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [Tab5Page],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Tab5Page);
+    component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+    fixture.detectChanges();
+  }));
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
