d8d0df56b89c745b4f54b3bb9a0642748b5e4480
alexandrebrugger <superalexbrugger@gmail.com>
2024-04-16 11:45:51 +0200

    derniers changements

--- mobile/src/app/tab5/tab5.page.spec.ts
+++ mobile/src/app/tab5/tab5.page.spec.ts
@@ -1,55 +1,26 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
-import { Tab5Page } from './tab5.page';
-
-describe('Tab5Page', () => {
-  let component: Tab5Page;
-  let fixture: ComponentFixture<Tab5Page>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [Tab5Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(Tab5Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});*/
-
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+
 import { Tab5Page } from './tab5.page';
 
 describe('Tab5Page', () => {
   let component: Tab5Page;
   let fixture: ComponentFixture<Tab5Page>;
-  let httpTestingController: HttpTestingController;
+  let httpMock : HttpTestingController;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab5Page],
-      imports: [IonicModule.forRoot(), HttpClientTestingModule]
+      declarations: [ Tab5Page ],
+      imports: [IonicModule.forRoot(),HttpClientTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab5Page);
     component = fixture.componentInstance;
-    httpTestingController = TestBed.inject(HttpTestingController);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   }));
-
-  afterEach(() => {
-    httpTestingController.verify();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
