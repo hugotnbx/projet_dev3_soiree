313ef2d19eccfbb8388faa22fdfa221d6abf668a
0ropo <he202225@students.ephec.be>
2024-05-05 21:30:31 +0200

    messages d'erreur + unique

--- mobile/src/app/pages/login/login.page.ts
+++ mobile/src/app/pages/login/login.page.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,17 +11,20 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private  authService:  AuthService, private  router:  Router, private localStorage: LocalStorageService) { }
-
+  constructor(private  authService:  AuthService, private  router:  Router, private localStorage: LocalStorageService,private location:Location) { }
+  isNotLogged=false
   ngOnInit() {}
 
   login(form:NgForm){
     this.authService.login(form.value).subscribe((res)=>{
       if(this.authService.isLoggedIn()){
+        this.isNotLogged=false
         this.router.navigateByUrl('');
       }
       //this.router.navigateByUrl('');
-    });
+    }, 
+    error => {this.isNotLogged=true}
+  );
   }
 
 }
