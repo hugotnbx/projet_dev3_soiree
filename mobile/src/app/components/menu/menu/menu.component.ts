0a42a8b1af59d0be646739bbe61c796d765038fc
Hugo Troonbeeckx <h.troonbeeckx@students.ephec.be>
2024-05-15 19:29:55 +0200

    Ajout confirmation de supression

--- mobile/src/app/components/menu/menu/menu.component.ts
+++ mobile/src/app/components/menu/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { environment } from 'src/environments/environment';
@@ -13,11 +14,37 @@ import { environment } from 'src/environments/environment';
 export class MenuComponent {
   userId :any;
 
-  constructor(public http:HttpClient, private authService: AuthService, private router: Router, private localStorage:LocalStorageService) {}
+  constructor(public http:HttpClient, private authService: AuthService, private router: Router, private localStorage:LocalStorageService, private alertController: AlertController) {}
 
   logout(){
     this.router.navigateByUrl('login');
     return this.authService.logout();
+  }
+
+  onClickUnsubscribe() {
+    // console.log('deleteButton:', this.deleteButton);
+    this.unsubscribeAlert();
+  }
+
+  async unsubscribeAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Êtes-vous sûr de vouloir supprimer votre compte ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirmer',
+          handler: () => {
+            this.unsubscribe();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   unsubscribe(){
